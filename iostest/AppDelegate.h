fe71f9f55ca50f6e2390c8ee69c9a9e6c438b21a
MisterZhouZhou <m15038960016@163.com>
2018-05-27 10:37:10 +0800

    update

---
+++ iostest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iostest
+//
+//  Created by 周巍的Mac on 2018/5/27.
+//  Copyright © 2018年 misterzhouzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iostest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iostest
+//
+//  Created by 周巍的Mac on 2018/5/27.
+//  Copyright © 2018年 misterzhouzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
